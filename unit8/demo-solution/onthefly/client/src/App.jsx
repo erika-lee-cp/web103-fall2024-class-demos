9fb9ba9bec5e09f57ac9adafc52c6aca6c205ff2
Erika Lee <erika.lee@codepath.org>
2024-11-07 14:37:00 -0800

    update unit 8 solutions

--- unit8/demo-solution/onthefly/client/src/App.jsx
+++ unit8/demo-solution/onthefly/client/src/App.jsx
@@ -15,16 +15,9 @@ const App = () => {
   
   const [trips, setTrips] = useState([])
   const [destinations, setDestinations] = useState([])
-  const [user, setUser] = useState([])
   const API_URL = 'http://localhost:3001'
 
   useEffect(() => {
-    const getUser = async () => {
-      const response = await fetch(`${API_URL}/auth/login/success`, { credentials: 'include' } )
-      const json = await response.json()
-      setUser(json.user)
-    }
-
     const fetchTrips = async () => {
       const response = await fetch(`${API_URL}/api/trips`)
       const data = await response.json()
@@ -37,76 +30,60 @@ const App = () => {
       setDestinations(data)
     }
 
-    getUser()
     fetchTrips()
     fetchDestinations()
   }, [])
 
-  const logout = async () => {
-    const url = `${API_URL}/auth/logout`
-    const response = await fetch(url, { credentials: 'include' })
-    const json = await response.json()
-    window.location.href = '/'
-  }
-
   let element = useRoutes([
     {
       path: '/',
-      element: user && user.id ?
-        <ReadTrips user={user} data={trips} /> : <Login api_url={API_URL} />
+      element:<ReadTrips data={trips} />
+    },
+    {
+      path: '/login',
+      element:<Login api_url={API_URL} />
     },
     {
       path: '/trip/new',
-      element: user && user.id ?
-        <CreateTrip user={user} api_url={API_URL} /> : <Login api_url={API_URL} />
+      element:<CreateTrip api_url={API_URL} />
     },
     {
       path: '/edit/:id',
-      element: user && user.id ?
-        <EditTrip user={user} data={trips} api_url={API_URL} /> : <Login api_url={API_URL} />
+      element: <EditTrip data={trips} />
     },
     {
       path: '/destinations',
-      element: user && user.id ?
-        <ReadDestinations user={user} data={destinations} /> : <Login api_url={API_URL} />
+      element: <ReadDestinations data={destinations} />
     },
     {
       path: '/trip/get/:id',
-      element: user && user.id ?
-        <TripDetails user={user} data={trips} api_url={API_URL} /> : <Login api_url={API_URL} />
+      element: <TripDetails data={trips} />
     },
     {
       path: '/destination/new/:trip_id',
-      element: user && user.id ?
-        <CreateDestination user={user} api_url={API_URL} /> : <Login api_url={API_URL} />
+      element: <CreateDestination />
     },
     {
       path: '/activity/create/:trip_id',
-      element: user && user.id ?
-        <CreateActivity user={user} api_url={API_URL} /> : <Login api_url={API_URL} />
+      element: <CreateActivity />
     },
     {
       path: '/destinations/add/:destination_id',
-      element: user && user.id ?
-        <AddToTrip user={user} data={trips} api_url={API_URL} /> : <Login api_url={API_URL} />
+      element: <AddToTrip data={trips}/>
     }
   ])
 
   return ( 
     <div className='App'>
-      {
-          user && user.id ?
-              <div className='header'>
-                  <h1>On The Fly ✈️</h1>
-                  <Link to='/'><button className='headerBtn'>Explore Trips</button></Link>
-                  <Link to='/destinations'><button className='headerBtn'>Explore Destinations</button></Link>
-                  <Link to='/trip/new'><button className='headerBtn'> + Add Trip </button></Link>
-                  <button onClick={logout} className='headerBtn'>Logout</button>
-              </div>
-          : <></>
-      }
 
-      {element}
+      <div className='header'>
+        <h1>On The Fly ✈️</h1>
+        <Link to='/'><button className='headerBtn'>Explore Trips</button></Link>
+        <Link to='/destinations'><button className='headerBtn'>Explore Destinations</button></Link>
+        <Link to='/trip/new'><button className='headerBtn'>+ Add Trip</button></Link>
+      </div>
+
+        {element}
     </div>
   )
 }
